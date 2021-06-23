195ee621481ec0df41be46432c225294068d86f0
eshan-chawla <75448641+Dead-Coder-007@users.noreply.github.com>
2021-06-24 01:10:49 +0530

    html platter
    +1

---
+++ Javascript_Logic/Unit_Conversion.js
@@ -0,0 +1,40 @@
+
+// For specific unit to other
+// Units and Abbreviation :
+// kg -> Kilograms
+// gm -> Grams
+// mg -> Millgrams
+// lbs -> Pounds
+// car -> Carrats
+// cg -> Centigrams
+// decig -> Decigrams
+// decag -> Deacgrams 
+// Hecg -> Hectograms
+// met_Ton -> Metric tonnes
+// oun -> Ounces
+// stn -> Stone
+// Short tons (US)
+// Long tons (UK)
+
+function Convert_weight_to_Specific(Input_Amount, Input_unit , Output_unit) 
+{
+    Conversion  = "Input_unit" + "Output_unit"
+    switch(Input_unit,Output_unit) {
+  case KG:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+    return p1 * p2;  
+}
+
+// For all available units
+function Convert_weight_to_All(Input_Amount , Input_unit)
+{
+    
+}
+
