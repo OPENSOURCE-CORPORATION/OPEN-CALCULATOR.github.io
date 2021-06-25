94818b572cc15142dcf887763b4569cb6bfa29bb
eshan-chawla <75448641+Dead-Coder-007@users.noreply.github.com>
2021-06-26 04:34:19 +0530

    Applied Free Opensource Template
    Template can be edited to fit for main page and CSS , J.s. files are added too for Design Purposes
    
    +1

--- Javascript_Logic/Unit_Conversion.js
+++ Javascript_Logic/Unit_Conversion.js
@@ -19,15 +19,16 @@
 function Convert_weight_to_Specific(Input_Amount, Input_unit, Output_unit) {
   var units = ["kg", "gm", "mg", "lbs", "car", "cg", "decig", "decag", "hecg", "met_Ton", "oun", "stn", "srt_Ton", "lng_Ton"];
   // To Get the conversion Factor b/w Units
-  var in_Kg = [1, 10 ^ 3, 10 ^ 6, 2.204623, 5000, 10 ^ 5, 10 ^ 4, 100, 10, 0.001, 35.27396, 0.157473, 0.001102, 0.000984]
-  var len = units.length;
-  console.log(len)
-  // for i in range()
-  //   return p1 * p2;  
+  var in_Kg = [1, 10 ^ 3, 10 ^ 6, 2.204623, 5000, 10 ^ 5, 10 ^ 4, 100, 10, 0.001, 35.27396, 0.157473, 0.001102, 0.000984];
+  var Output;
+  var len = 14;
+  var 
+  return 
 }
 
 // For all available units
 function Convert_weight_to_All(Input_Amount, Input_unit) {
 
 }
+Convert_weight_to_Specific()
 
